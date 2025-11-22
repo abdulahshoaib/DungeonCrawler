ba444f15c61ce0b64105320ff4b9febd62579d86
ali-2004-byte <ah5231114@gmail.com>
2025-11-22 09:12:07 +0500

    feat: Load Menu

---
+++ Menu.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <raylib.h>
+#include "UIElement.h"
+#include <utils.h>
+
+class Menu {
+protected:
+  List<UIElement> elements;
+
+public:
+  Menu();
+  virtual ~Menu();
+  virtual void Draw();
+};
