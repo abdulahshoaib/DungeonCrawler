d2f102c7a297af6faf44c2283dac983ea551c3a0
ali-2004-byte <ah5231114@gmail.com>
2025-11-22 09:13:48 +0500

    feat: Load Player

---
+++ Player.cpp
@@ -0,0 +1,13 @@
+
+#include "Player.h"
+
+Player::Player(Vector2 position, Vector2 size) : pos(position), size(size) {}
+Player::Player() : pos({0, 0}), size({10, 10}) {}
+
+Vector2 Player::getPos() const { return pos; }
+Vector2 Player::getSize() const { return size; }
+
+void Player::move(float _x, float _y) {
+  pos.x += _x;
+  pos.y += _y;
+}
