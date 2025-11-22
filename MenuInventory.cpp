10a98d221585605c9083616c36600f0f75365216
ali-2004-byte <ah5231114@gmail.com>
2025-11-22 09:12:43 +0500

    feat: Load MenuInventory

---
+++ MenuInventory.cpp
@@ -0,0 +1,5 @@
+
+#include "MenuInventory.h"
+
+MenuInventory::MenuInventory(Font *font) {}
+void MenuInventory::Draw() {}
---
+++ MenuInventory.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Menu.h"
+
+class MenuInventory : public Menu {
+public:
+  MenuInventory(Font *);
+  void Draw();
+};
