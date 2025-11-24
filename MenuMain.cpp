1b917ac85ed171837db94e072d3a44fbfb7714ef
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-24 16:26:48 +0500

    feat: moved the main menu logic of drawing and input handling to the MenuMain class rather than the run class of the engine

--- MenuMain.cpp
+++ MenuMain.cpp
@@ -1,18 +1,16 @@
+#include "Engine.h"
 #include "MenuMain.h"
 #include "Button.h"
 MenuMain::MenuMain()
 {
-  Button Play_btn, Pause_btn, Settings_btn, Quit;
   elements.Append(Play_btn);
-  elements.Append(Pause_btn);
+  elements.Append(Quit_btn);
   elements.Append(Settings_btn);
 }
 
 void MenuMain::Draw()
 {
   Vector2 mousePoint = GetMousePosition();
-  bool clicked = IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
-
   int screenWidth = 1100;
 
   int cx = screenWidth / 2;
@@ -20,9 +18,9 @@ void MenuMain::Draw()
   int buttonFontSize = 20;
   int titleFontSize = 60;
 
-  Vector2 titleText = {cx - 250, cy};
+  Vector2 titleText = {cx - 230, cy};
 
-  Rectangle PlayRect = {cx - (int)(200 / 2), titleText.y + titleFontSize * 2, 200, 50};
+  Rectangle PlayRect = {cx - (int)(200 / 2), titleText.y + titleFontSize * 4, 200, 50};
   Rectangle SettingsRect = {cx - (int)200 / 2, PlayRect.y + PlayRect.height + 10, 200, 50};
   Rectangle QuitRect = {cx - (int)200 / 2, SettingsRect.y + SettingsRect.height + 10, 200, 50};
 
@@ -32,11 +30,6 @@ void MenuMain::Draw()
   if (CheckCollisionPointRec(mousePoint, PlayRect))
   {
     playColor = GREEN;
-
-    if (clicked)
-    {
-      // TODO: engine->gameState = GAME_LOOP;
-    }
   }
   DrawRectangleRec(PlayRect, playColor);
   DrawText("PLAY", PlayRect.x + 70, PlayRect.y + 15, buttonFontSize, WHITE);
@@ -45,11 +38,6 @@ void MenuMain::Draw()
   if (CheckCollisionPointRec(mousePoint, SettingsRect))
   {
     settingsColor = PURPLE;
-
-    if (clicked)
-    {
-      // TODO: game state change
-    }
   }
   DrawRectangleRec(SettingsRect, settingsColor);
   DrawText("SETTINGS", SettingsRect.x + 45, SettingsRect.y + 15, buttonFontSize, WHITE);
@@ -58,12 +46,28 @@ void MenuMain::Draw()
   if (CheckCollisionPointRec(mousePoint, QuitRect))
   {
     quitColor = RED;
-
-    if (clicked)
-    {
-      // TODO: game state change
-    }
   }
   DrawRectangleRec(QuitRect, quitColor);
   DrawText("QUIT", QuitRect.x + 70, QuitRect.y + 15, buttonFontSize, WHITE);
+}
+
+void MenuMain::HandleInput(Engine &engine)
+{
+  bool clicked = IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+
+  Vector2 mousePoint = GetMousePosition();
+  if (clicked && CheckCollisionPointRec(mousePoint, Play_btn.rect))
+  {
+    engine.gameState = new PlayState();
+  }
+
+  if (clicked && CheckCollisionPointRec(mousePoint, Settings_btn.rect))
+  {
+    engine.gameState = new SettingsState();
+  }
+
+  if (clicked && CheckCollisionPointRec(mousePoint, Quit_btn.rect))
+  {
+    CloseWindow();
+  }
 }
--- MenuMain.h
+++ MenuMain.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "Menu.h"
 
-class MenuMain : public Menu {
+class MenuMain : public Menu
+{
+private:
+  Button Play_btn, Settings_btn, Quit_btn;
+
 public:
   MenuMain();
   void Draw();
+  void HandleInput(Engine &) override;
 };
