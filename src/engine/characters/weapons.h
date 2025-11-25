f9ced64b2a94ee22510ca1cad9a8adf0d12665a4
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-26 00:47:45 +0500

    feat: add WeaponType enum for character weapons

---
+++ src/engine/characters/weapons.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum class WeaponType
+{
+    SWORD,  // Knight's primary weapon
+    ARROW,  // Samurai_Archer's secondary weapon
+    KATANA, // All Samurai's primary weapon
+    SPEAR,  // Skeleon's primary weapon
+    KNIFE,  // Yokai's primary weapon
+};
