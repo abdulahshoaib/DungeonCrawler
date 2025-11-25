147790fd892d32ab9b044b07257e6734baabcb23
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-25 14:03:55 +0500

    feat: added mute button to settings page

--- src/ui/menus/MenuSettings.cpp
+++ src/ui/menus/MenuSettings.cpp
@@ -1,6 +1,5 @@
-#pragma once
-
 #include "MenuSettings.h"
+#include "Loader.h"
 
 MenuSettings::MenuSettings()
 {
@@ -8,6 +7,10 @@ MenuSettings::MenuSettings()
 
 void MenuSettings::Draw()
 {
+    Texture2D bg = Loader::MainMenuBackground;
+    Vol_btn.label.text = "Mute";
+    Vol_btn.rect = {100, 100, 200, 50};
+    Vol_btn.Draw(GREEN, DARKGREEN);
 }
 
 void MenuSettings::HandleInput(Engine &)
--- src/ui/menus/MenuSettings.h
+++ src/ui/menus/MenuSettings.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "Menu.h"
+#include "ui/ui_elements/Button.h"
+#include "ui/ui_elements/Label.h"
 
 class MenuSettings : public Menu
 {
+private:
+    Button Vol_btn;
+    Label Settings_lbl;
+
 public:
     MenuSettings();
     void Draw();
