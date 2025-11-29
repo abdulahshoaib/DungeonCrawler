2f235db932ba831c1fcbdeb83d6c2c86bc2de640
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-29 21:41:04 +0500

    feat: implement animation state handling for SamuraiCommander

--- src/engine/characters/SamuraiCommander.cpp
+++ src/engine/characters/SamuraiCommander.cpp
@@ -19,4 +19,50 @@ SamuraiCommander::SamuraiCommander()
 
 void SamuraiCommander::anim()
 {
+    switch (animState)
+    {
+    case AnimState::IDLE:
+        currentAnim = idle1Anim;
+        break;
+
+    case AnimState::RUN:
+        currentAnim = runAnim;
+        break;
+
+    case AnimState::WALK:
+        currentAnim = walkAnim;
+        break;
+
+    case AnimState::JUMP:
+        currentAnim = jumpAnim;
+        break;
+
+    case AnimState::ATTACK1:
+        currentAnim = attack1Anim;
+        break;
+
+    case AnimState::ATTACK2:
+        currentAnim = attack2Anim;
+        break;
+
+    case AnimState::ATTACK3:
+        currentAnim = attack3Anim;
+        break;
+
+    case AnimState::HURT:
+        currentAnim = hurtAnim;
+        break;
+
+    case AnimState::DEAD:
+        currentAnim = deadAnim;
+        break;
+
+    case AnimState::PROTECT:
+        currentAnim = protectAnim;
+        break;
+
+    default:
+        currentAnim = idle1Anim;
+        break;
+    }
 }
