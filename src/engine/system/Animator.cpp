fddc7f061f1d0e9fa291fd46b1aae478d2c38a11
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-29 18:50:40 +0500

    refactor: add Animator class with basic structure and methods

---
+++ src/engine/system/Animator.cpp
@@ -0,0 +1,18 @@
+#include "Animator.h"
+
+Animator::Animator()
+{
+}
+
+Animator::~Animator()
+{
+}
+
+void Animator::Update(Character *c, float)
+{
+    // if (c->)
+}
+
+void Animator::Draw(Character *)
+{
+}
