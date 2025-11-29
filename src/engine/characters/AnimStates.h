f0d2e19a83f08241d4a082818ea3bd61f741a325
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-29 18:44:26 +0500

    feat: added the animation state enum representing animation the animator is rendering for the charachter

---
+++ src/engine/characters/AnimStates.h
@@ -0,0 +1,34 @@
+#pragma once
+
+enum AnimState
+{
+    IDLE,
+
+    WALK,
+
+    RUN,
+
+    JUMP,
+
+    RUN_ATTACK,
+
+    ATTACK1,
+    ATTACK2,
+    ATTACK3,
+
+    DEAD,
+
+    HURT,
+
+    PROTECT,
+
+    SHOT1,
+    SHOT2,
+
+    CHARGE,
+    EVASION,
+
+    FALL,
+
+    FIRE,
+};
