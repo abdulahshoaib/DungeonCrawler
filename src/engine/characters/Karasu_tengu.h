be65367587c6d9adcc44f96b9ceaa903e6f3fa41
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-29 18:48:44 +0500

    refactor: streamline Karasu_tengu class by removing texture loading methods and consolidating animation initialization

--- src/engine/characters/Karasu_tengu.h
+++ src/engine/characters/Karasu_tengu.h
@@ -7,7 +7,5 @@ class Karasu_tengu : public Character
 {
 public:
     Karasu_tengu();
-    void LoadTex() override;
-    void UnloadTex() override;
-    ~Karasu_tengu();
+    void anim() override;
 };
